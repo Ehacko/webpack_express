054816669ddeea624f6241cbfe747e82f2fe0f7b
Ehacko <darkdav.t@gmail.com>
2021-05-15 00:01:12 -0400

    banner création

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const router = express.Router();
-
+const routes = [
+  {path: '', render: }
+]
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+for(route in ["menus", "recettes", "search"]) {
+  router.get(`/${route}`, function(req, res, next) {
+    res.render('index', { title: route });
+  });
+}
 
 module.exports = router;
