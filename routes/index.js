7616b38ddd7b5d70f05e680273ad1d81c1c6c686
Ehacko <darkdav.t@gmail.com>
2021-05-15 00:09:20 -0400

    a

--- routes/index.js
+++ routes/index.js
@@ -5,11 +5,11 @@ const router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-
+/*
 for(route in ["menus", "recettes", "search"]) {
   router.get(`/${route}`, function(req, res, next) {
     res.render('index', { title: route });
   });
 }
-
+*/
 module.exports = router;
